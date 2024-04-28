6a1a6177ea97c68f548306641ddfdf396d457504
Louis Grange <70532216+l0uisgrange@users.noreply.github.com>
2024-04-28 22:16:33 +0200

    Fixed mailbox list

---
+++ README.md
@@ -0,0 +1,14 @@
+# @simpleimap/server
+
+![NPM Version](https://img.shields.io/npm/v/%40simpleimap%2Fserver?style=for-the-badge&logo=npm&color=blue&label=version)
+![NPM Downloads](https://img.shields.io/npm/dy/%40simpleimap%2Fserver?style=for-the-badge&logo=npm)
+
+SimpleIMAP is a robust, easy to use IMAP Server. Built with Node.js, it offers high performance and a straightforward interface for setting up and launching your IMAP server.
+
+#### Installation
+
+This package support
+
+```
+npm install @simpleimap/server
+```
--- app.ts
+++ app.ts
@@ -8,7 +8,8 @@ export default function imap({port=3000, host='localhost', capabilities=['AUTH=P
     capabilities?: Capability[],
     debug?: boolean
 }) {
-    const server = net.createServer({allowHalfOpen: true, keepAlive: true}, (socket) => {
+    const server = net.createServer({allowHalfOpen: true, keepAlive: true}, (socket: any) => {
+        let loggedIn: boolean = false
         socket.setEncoding('utf8')
         socket.write(`* OK [CAPABILITY IMAP4rev2 ${capabilities.join(' ')}] Server ready\n`)
         socket.on('data', (data: string) => {
@@ -22,17 +23,35 @@ export default function imap({port=3000, host='localhost', capabilities=['AUTH=P
                     socket.write(`${tag} OK LOGOUT completed\n`)
                     socket.destroySoon()
                     break
+                case 'SELECT':
+                    if (loggedIn) {
+
+                        socket.write(`${tag} OK SELECT completed\n`)
+                    } else {
+                        socket.write(`${tag} NO [ALERT] authentication required\n`)
+                    }
+                    break
                 case 'LIST':
-                    socket.write('* LIST (\\Noselect) "/" ""\n')
-                    socket.write(`${tag} OK LIST completed\n`)
+                    if (loggedIn) {
+                        socket.write(`* LIST (\\NoInferiors) "/" INBOX \n`)
+                        socket.write(`* LIST (\\NoInferiors \\Drafts) "/" Drafts \n`)
+                        socket.write(`* LIST (\\NoInferiors \\Sent) "/" Sent \n`)
+                        socket.write(`* LIST (\\NoInferiors \\Junk) "/" Junk \n`)
+                        socket.write(`* LIST (\\NoInferiors \\Trash) "/" Trash \n`)
+                        socket.write(`* LIST (\\NoInferiors \\Archive) "/" Archives \n`)
+                        socket.write(`${tag} OK LIST completed\n`)
+                    } else {
+                        socket.write(`${tag} NO [ALERT] authentication required\n`)
+                    }
                     break
                 case 'CAPABILITY':
                     socket.write(`* CAPABILITY IMAP4rev2 ${capabilities.join(" ")}\n`)
                     socket.write(`${tag} OK CAPABILITY completed\n`)
                     break
                 case 'LOGIN':
-                    if (options.length >= 2) {
+                    if (options.length == 2) {
                         if (options[0] === 'PAUL' && options[1] === 'MICHEL') {
+                            loggedIn = true
                             socket.write(`${tag} OK LOGIN completed\n`)
                         } else {
                             socket.write(`${tag} NO LOGIN failure\n`)
@@ -54,14 +73,7 @@ export default function imap({port=3000, host='localhost', capabilities=['AUTH=P
             console.log('Connection ended')
         })
     })
-    server.listen({port: port, host: host, exclusive: true}, () => {
-        console.log(` \x1b[42m START \x1b[0m IMAP Server is running\n`)
-        console.log(`\x1b[1m local \x1b[0m     imap://localhost:${port}`)
-        console.log(`\x1b[1m network \x1b[0m  \x1b[37m disabled\x1b[0m\n`)
-    })
-    server.on('error', (error) => {
+    server.on('error', (error: any) => {
         console.error(` \x1b[41m ERROR \x1b[0m ${error.message}\n`)
     })
 }
-
-imap({})
